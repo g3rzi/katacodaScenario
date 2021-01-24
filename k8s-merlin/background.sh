f6a479ad6feb70539f2971c8db675c2eabbdf5a5
Evi <evyatar575@gmail.com>
2021-01-24 17:36:06 +0200

    update

--- k8s-merlin/background.sh
+++ k8s-merlin/background.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 touch /root/aaaaaaaaa
-wget https://github.com/g3rzi/katacodaScenario/releases/download/0.1/merlin.tar
-tar -xf merlin.tar
-cd merlin
+#wget https://github.com/g3rzi/katacodaScenario/releases/download/0.1/merlin.tar
+#tar -xf merlin.tar
+#cd merlin
