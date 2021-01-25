19aea3dc2a6d4cc487065429ab22517012e03d09
Evi <evyatar575@gmail.com>
2021-01-25 11:11:46 +0200

    update

--- k8s-merlin/background.sh
+++ k8s-merlin/background.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 touch /root/aaaaaaaaa
+HOST=`hostname`
+if [ "$HOST" == "controlplane" ]; then
+	touch /root/thisisthehost
+fi
 #wget https://github.com/g3rzi/katacodaScenario/releases/download/0.1/merlin.tar
 #tar -xf merlin.tar
 #cd merlin
