c3c219eeb4bff72b422021031c9eb363ee95a584
Evi <evyatar575@gmail.com>
2021-01-27 19:29:37 +0200

    update

--- k8s-merlin/background.sh
+++ k8s-merlin/background.sh
@@ -4,6 +4,9 @@
 #HOST_IP=`kubectl get nodes -o jsonpath='{ $.items[?(@.metadata.name=="controlplane")].status.addresses[?(@.type=="InternalIP")].address }'` && sed 's/    Interface: "127.0.0.1"/    Interface: '"$HOST_IP"'/g' config.yaml`{{execute}}
 #Run the file `./merlinServer-Linux-x64
 
-./launch.sh
-
-kubectl run priv-container --image=alpine --overrides='{"spec": {"template": {"spec": {"containers": [{"name": "priv-container", "image": "alpine", "command": ["sh", "-c", "sleep 1000"], "securityContext": {"privileged": true} }]}}}}' -- sh -c 'sleep 1000'
+HOST=`hostname`
+if [ "$HOST" == "controlplane" ]; then
+	  echo "This is the Host"
+	  ./launch.sh
+	  kubectl run priv-container --image=alpine --overrides='{"spec": {"template": {"spec": {"containers": [{"name": "priv-container", "image": "alpine", "command": ["sh", "-c", "sleep 1000"], "securityContext": {"privileged": true} }]}}}}' -- sh -c 'sleep 1000'
+fi
