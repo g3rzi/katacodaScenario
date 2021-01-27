83c6f66660d870912f75125574e2bb785119cdd2
Evi <evyatar575@gmail.com>
2021-01-27 17:59:16 +0200

    update

--- k8s-merlin/foreground.sh
+++ k8s-merlin/foreground.sh
@@ -5,9 +5,9 @@ touch /root/aaaaaaaaa
 HOST=`hostname`
 if [ "$HOST" == "controlplane" ]; then
 	  echo "This is the Host"
-	  #wget https://github.com/g3rzi/katacodaScenario/releases/download/0.1/merlinServerAndData.tar
+	  wget https://github.com/g3rzi/katacodaScenario/releases/download/0.1/merlinServerAndData.tar
 	  mkdir /root/merlin
-	  #tar -xf merlinServerAndData.tar --directory /root/merlin
+	  tar -xf merlinServerAndData.tar --directory /root/merlin
 	  cd merlin
 	  cat <<EOT >> config.yaml
 AutoStart: true
@@ -21,7 +21,7 @@ EOT
       sed -i 's/    Interface: "127.0.0.1"/    Interface: '"$HOST_IP"'/g' config.yaml
 
 	else
-      #wget https://github.com/g3rzi/katacodaScenario/releases/download/0.1/merlinAgent-Linux-x64
+      wget https://github.com/g3rzi/katacodaScenario/releases/download/0.1/merlinAgent-Linux-x64
       echo "This is the Worker"
 fi
 
