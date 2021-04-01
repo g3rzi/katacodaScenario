ced0ff58efbb4c091d72ee686a9b9c6a1ab99968
Eviatar <eviatar.gerzi@cyberark.com>
2021-04-01 17:24:47 +0300

    upate

--- kubesploit/background.sh
+++ kubesploit/background.sh
@@ -2,7 +2,7 @@
 #HOST_IP=`ip addr show ens3  | awk '$1 == "inet" { print $2 }' | cut -d/ -f1`
 #sed 's/    Interface: "127.0.0.1"/    Interface: '"$HOST_IP"'/g' config.yaml
 #HOST_IP=`kubectl get nodes -o jsonpath='{ $.items[?(@.metadata.name=="controlplane")].status.addresses[?(@.type=="InternalIP")].address }'` && sed 's/    Interface: "127.0.0.1"/    Interface: '"$HOST_IP"'/g' config.yaml`{{execute}}
-#Run the file `./merlinServer-Linux-x64
+#Run the file `./kubesploitAgent-Linux-x64_katacoda
 
 HOST=`hostname`
 if [ "$HOST" == "controlplane" ]; then
